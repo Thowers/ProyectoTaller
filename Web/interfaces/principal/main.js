d768af42bb62d5410cd6a88040b11030b1f55c88
Crimen. <sebastiianlopez021016@gmail.com>
2024-11-08 23:35:06 -0500

    Cambios

--- Web/interfaces/principal/main.js
+++ Web/interfaces/principal/main.js
@@ -1,17 +1,3 @@
-// Función de búsqueda (simulación)
-function buscar() {
-    let terminoBusqueda = document.getElementById("searchInput").value;
-    if (terminoBusqueda) {
-        alert("Buscando: " + terminoBusqueda);
-    } else {
-        alert("Por favor ingresa un término de búsqueda.");
-    }
-}
-
-// Función para el botón de "Explora Ahora" (lleva a la sección de Artistas)
-function explora() {
-    window.location.href = "#artistas";
-}
 // Seleccionar el contenedor del popup y los elementos de imagen, título y texto
 const popup = document.getElementById("popup");
 const popupImage = document.getElementById("popup-image");
